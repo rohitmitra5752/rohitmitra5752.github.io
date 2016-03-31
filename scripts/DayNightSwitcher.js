e60f4500b58de2d887072bf2d33c2f3d0a665274
Rohit Mitra <rohitmitra5752@gmail.com>
2016-03-31 18:29:02 +0530

    Text Switcher And Animation
    Adding Text Switcher and Animation to the existing functionality

--- scripts/DayNightSwitcher.js
+++ scripts/DayNightSwitcher.js
@@ -1,9 +1,23 @@
 $(function(){
-	$("#dayNightSwitcher > #dnSwitcherBox").change(function(){
+	$("#dayNightSwitcher > .dayNightIconSwitcher > .dnSwitcherBox").change(function(){
 		if (this.checked) {
 			$("body").removeClass("day").addClass("night");
+			$(this).parent().siblings(".dayNightTextSwitcher").addClass("hideDay");
 		} else {
 			$("body").removeClass("night").addClass("day");
+			$(this).parent().siblings(".dayNightTextSwitcher").removeClass("hideDay");
+		}
+	});
+	
+	$("#dayNightSwitcher > .dayNightTextSwitcher").click(function(){
+		if ($(this).hasClass("hideDay")) {
+			$(this).removeClass("hideDay");
+			$("body").removeClass("night").addClass("day");
+			$(this).siblings(".dayNightIconSwitcher").children(".dnSwitcherBox").prop('checked',false);
+		} else {
+			$(this).addClass("hideDay");
+			$("body").removeClass("day").addClass("night");
+			$(this).siblings(".dayNightIconSwitcher").children(".dnSwitcherBox").prop('checked',true);
 		}
 	});
 });
